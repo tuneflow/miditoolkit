4ae5287465176da666e61a6ef22cfa202074cf23
Andantei <zaoyinji@gmail.com>
2023-02-16 16:09:01 -0800

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='miditoolkit_light',
-    version='0.1.18',
+    version='0.1.19',
     description='',
     author='TuneFlow',
     author_email='contact@info.tuneflow.com',
